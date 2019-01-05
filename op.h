ae3ab90a57909452020282f212c35f2f4f8c80a8
Oleksandr KRYZHANOVSKYI <okryzhan@e2r11p13.unit.ua>
2019-01-05 17:10:13 +0200

    op tab added

--- op.h
+++ op.h
@@ -18,6 +18,8 @@
 #ifndef OP_H
 # define OP_H
 
+# include <stdint.h>
+
 # define IND_SIZE				2
 # define REG_SIZE				4
 # define DIR_SIZE				REG_SIZE
@@ -49,20 +51,12 @@
 # define NBR_LIVE				21
 # define MAX_CHECKS				10
 
-/*
-** C
-*/
-
 typedef char	t_arg_type;
 
 # define T_REG					1
 # define T_DIR					2
 # define T_IND					4
 # define T_LAB					8
-
-/*
-** Constants
-*/
 
 # define PROG_NAME_LENGTH		(128)
 # define COMMENT_LENGTH			(2048)
@@ -75,5 +69,129 @@ typedef struct					s_header
 	unsigned int				prog_size;
 	char						comment[COMMENT_LENGTH + 1];
 }								t_header;
+
+typedef struct					s_op
+{
+	char		*name;
+	uint8_t		code;
+	uint8_t		args_num;
+	uint8_t		args_types[3];
+	uint8_t		t_dir_size;
+}								t_op;
+
+t_op	g_op[16] = {
+	{
+		.name = "live",
+		.code = 0x01,
+		.args_num = 1,
+		.args_types = {T_DIR | 0 | 0},
+		.t_dir_size = 4
+	},
+	{
+		.name = "ld",
+		.code = 0x02,
+		.args_num = 2,
+		.args_types = {T_DIR | T_IND, T_REG, 0},
+		.t_dir_size = 4,
+	},
+	{
+		.name = "st",
+		.code = 0x03,
+		.args_num = 2,
+		.args_types = {T_REG, T_REG | T_IND, 0},
+		.t_dir_size = 4,
+	},
+	{
+		.name = "add",
+		.code = 0x04,
+		.args_num = 3,
+		.args_types = {T_REG, T_REG, T_REG},
+		.t_dir_size = 4,
+	},
+	{
+		.name = "sub",
+		.code = 0x05,
+		.args_num = 3,
+		.args_types = {T_REG, T_REG, T_REG},
+		.t_dir_size = 4,
+	},
+	{
+		.name = "and",
+		.code = 0x06,
+		.args_num = 3,
+		.args_types = {T_REG | T_DIR | T_IND, T_REG | T_DIR | T_IND, T_REG},
+		.t_dir_size = 4,
+	},
+	{
+		.name = "or",
+		.code = 0x07,
+		.args_num = 3,
+		.args_types = {T_REG | T_DIR | T_IND, T_REG | T_DIR | T_IND, T_REG},
+		.t_dir_size = 4,
+	},
+	{
+		.name = "xor",
+		.code = 0x08,
+		.args_num = 3,
+		.args_types = {T_REG | T_DIR | T_IND, T_REG | T_DIR | T_IND, T_REG},
+		.t_dir_size = 4,
+	},
+	{
+		.name = "zjmp",
+		.code = 0x09,
+		.args_num = 1,
+		.args_types = {T_DIR, 0, 0},
+		.t_dir_size = 2,
+	},
+	{
+		.name = "ldi",
+		.code = 0x0A,
+		.args_num = 3,
+		.args_types = {T_REG | T_DIR | T_IND, T_REG | T_DIR, T_REG},
+		.t_dir_size = 2,
+	},
+	{
+		.name = "sti",
+		.code = 0x0B,
+		.args_num = 3,
+		.args_types = {T_REG, T_REG | T_DIR | T_IND, T_REG | T_DIR},
+		.t_dir_size = 2,
+	},
+	{
+		.name = "fork",
+		.code = 0x0C,
+		.args_num = 1,
+		.args_types = {T_DIR, 0, 0},
+		.t_dir_size = 2,
+	},
+	{
+		.name = "lld",
+		.code = 0x0D,
+		.args_num = 2,
+		.args_types = {T_DIR | T_IND, T_REG, 0},
+		.t_dir_size = 4,
+	},
+	{
+		.name = "lldi",
+		.code = 0x0E,
+		.args_num = 3,
+		.args_types = {T_REG | T_DIR | T_IND, T_REG | T_DIR, T_REG},
+		.t_dir_size = 2,
+	},
+	{
+		.name = "lfork",
+		.code = 0x0F,
+		.args_num = 1,
+		.args_types = {T_DIR, 0, 0},
+		.t_dir_size = 2,
+	},
+	{
+		.name = "aff",
+		.code = 0x10,
+		.args_num = 1,
+		.args_types = {T_REG, 0, 0},
+		.t_dir_size = 4,
+	}
+};
 
 #endif
