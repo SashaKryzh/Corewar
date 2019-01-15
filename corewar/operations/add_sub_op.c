3bb3bbf97e8c3754f3710bf9f21f38c6fc9df9e2
Oleksandr KRYZHANOVSKYI <okryzhan@e1r5p9.unit.ua>
2019-01-15 13:27:30 +0200

    st_op

---
+++ corewar/operations/add_sub_op.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   add_sub_op.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: okryzhan <okryzhan@student.unit.ua>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/15 13:12:31 by okryzhan          #+#    #+#             */
+/*   Updated: 2019/01/15 13:12:31 by okryzhan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "corewar.h"
+
+void		add_sub_op(uint8_t *arena, t_car *car)
+{
+	int reg_nums[3];
+	int	to_put;
+	int i;
+
+	i = 0;
+	while (i < 3)
+	{
+		reg_nums[i] = get_reg_num(arena, car, i + 1);
+		i++;
+	}
+	if (car->op == 0x04)
+		to_put = car->regs[reg_nums[0] - 1] + car->regs[reg_nums[1] - 1];
+	else
+		to_put = car->regs[reg_nums[0] - 1] - car->regs[reg_nums[1] - 1];
+	car->regs[reg_nums[2] - 1] = to_put;
+	car->carry = to_put == 0 ? 1 : 0;
+}
---
+++ corewar/operations/st_op.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   st_op.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: okryzhan <okryzhan@student.unit.ua>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/15 13:25:45 by okryzhan          #+#    #+#             */
+/*   Updated: 2019/01/15 13:25:45 by okryzhan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "corewar.h"
+
+void		st_op(uint8_t *arena, t_car *car)
+{
+	int reg_num;
+	int	where;
+
+	reg_num = get_reg_num(arena, car, 1);
+	if (car->args_types[1] == REG_CODE)
+	{
+		where = get_reg_num(arena, car, 2);
+		car->regs[where - 1] = car->regs[reg_num - 1];
+	}
+	else
+	{
+		where = get_value(arena, to_arg(arena, car, 2), IND_SIZE) % IDX_MOD;
+		where = (MEM_SIZE + car->position + where) % MEM_SIZE;
+		put_on_arena(arena, where, (uint8_t *)(&car->regs[reg_num - 1]), REG_SIZE);
+	}
+}
